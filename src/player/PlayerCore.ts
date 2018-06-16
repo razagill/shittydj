85a675838ca9c109dfd1ff332d4aefef95a5d5b8
Boris Rutkowski <boris.rutkowski@mantro.net>
2018-06-16 21:06:55 +0200

    blubb

--- src/player/PlayerCore.ts
+++ src/player/PlayerCore.ts
@@ -1,10 +1,18 @@
+const lame = require('lame');
 const Speaker = require('speaker');
+const ffmpeg = require('fluent-ffmpeg');
 
 export default class PlayerCore {
-  private speaker = new Speaker();
 
   public play = (stream:any) => {
-    stream.pipe(this.speaker);
+    ffmpeg(stream)
+      .format('mp3')
+      .pipe(
+        new lame.Decoder()
+      )
+      .on('format', function (format) {
+        this.pipe(new Speaker(format));
+      });
   }
 
   public pause = () => {
--- src/providers/youtube/youtube.provider.ts
+++ src/providers/youtube/youtube.provider.ts
@@ -2,21 +2,15 @@ import * as ytdl from 'ytdl-core';
 
 export default class YoutubeProvider {
 
-  public getStream = (url: string) => {
+  public getStream = async (url: string) => {
+    const opt = {
+      videoFormat: 'mp4',
+      quality: 'lowest',
+      audioFormat: 'mp3',
+      filter: (format) => format.container === 'mp4' && format.audioEncoding,
+    };
 
-    return ytdl.getInfo(url, (err, info) => {
-      if (err) throw err;
-      var audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
-      if (audioFormats.length) {
-        const opt = {
-          filter: (format) => format.container === 'audioonly'
-        };
-        return ytdl(audioFormats[0].url, opt);
-      }
-      console.log('Could not find audio format for this link');
-    })
-
-
+    return ytdl(url, opt);
   }
 
 }

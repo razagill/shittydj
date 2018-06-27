c0fc2acb867ac5292a4d40c9d290aa6323262148
Raza Gill <raza.gill@outlook.com>
2018-06-27 11:18:53 +0200

    :zap: add build script

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import * as rootPath from 'app-root-path';
 
 class App {
-
   public app:express.Application;
+
   constructor() {
     this.app = express();
     this.config();
   }
 
   private config = () => {
-    this.app.set('views', './src/views')
+    this.app.set('views', `${rootPath}/src/views`)
     this.app.set('view engine', 'ejs')
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: false }));

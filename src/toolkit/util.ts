1fe69428e7afc41828289646521bb1bd22d95fcf
Boris Rutkowski <boris.rutkowski@mantro.net>
2018-07-16 12:21:37 +0200

    added a getProviderOfLink function at util.ts

--- src/toolkit/util.ts
+++ src/toolkit/util.ts
@@ -8,3 +8,16 @@ export const readFileLineByLine = (pathToFile: string) => {
   const arr = fs.readFileSync(pathToFile).toString().split('\n');
   return arr.filter((entry) => entry !== '');
 }
+
+export const getProviderOfLink = (link: string) => {
+  // check for youtube
+  if (link.indexOf('youtube') > -1 || link.indexOf('youtu.be') > -1) {
+    return 'Youtube';
+  }
+  // check for bandcamp
+  if (link.indexOf('bandcamp') > -1) {
+    return 'Bankcamp';
+  }
+  logger(`Unknown Provider found at ${link}`);
+  return 'unknown';
+}
